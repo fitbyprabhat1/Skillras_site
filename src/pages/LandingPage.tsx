562bf6fc54d716a70deac6847cd318a423c3b4f0
fitbyprabhat1 <fitbyprabhat@gmail.com>
2025-07-12 14:34:12 +0530

    Updated LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -10,13 +10,8 @@ import FAQSection from '../sections/FAQSection';
 import FooterSection from '../sections/FooterSection';
 import FloatingCTA from '../components/FloatingCTA';
 import ScrollToTop from '../components/ScrollToTop';
-import WelcomePopup from '../components/WelcomePopup';
 
 const LandingPage: React.FC = () => {
-  const handleFormSubmit = (data: { name: string; email: string; phone: string }) => {
-    console.log('Lead data submitted successfully:', data);
-    // You can add additional logic here like tracking, analytics, etc.
-  };
 
   return (
     <div className="min-h-screen bg-dark text-white">
